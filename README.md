51b8b478280eb431144a944eaf5d053e537b9eb7
rexwang8 <rexkingsbackyard@gmail.com>
2023-01-25 00:49:14 -0500

    added params

--- README.md
+++ README.md
@@ -9,10 +9,28 @@ Contents:
 
 This script downloads plain text files of Western Romance books publicaly avaible on [Smashworks](https://www.smashwords.com/). This website has been used to create popular Machine Learning datasets like [BookCorpus](https://huggingface.co/datasets/bookcorpus).
 
-The source code located in `cmd/smashwords-downloader`.
+The source code located in `cmd/smashwords-downloader`. 
+It can be built into an executable with the command `go build *.go`.
 
 The `main.go` script takes the following arugments:
 ```
   -data_dir string
         directory that the book files will download to (default "./data")
+  
+  -id integer
+        The cooresponding ID for the smashswords url you want to scrape
+        https://www.smashwords.com/books/category/1105/downloads/0/free would have an ID of 1105 (default is 1245 == western romance)
+
+  -pageitems integer
+        The number of items smashword has per page, shouldn't need to be changed. (default is 20)
+
+  -pages integer
+        The number of pages you want to download. (default is 7)
+
+  -format string
+        The format of text you want to download, some books only have limited format avaliability.
+        (default is txt for .txt files), options are (txt, epub)
 ```
+
+Example Execution
+./main -data_dir data2 -id 1105 -pageitem 20 -pages 10 -format txt
--- cmd/smashwords-downloader/main.go
+++ cmd/smashwords-downloader/main.go
@@ -21,16 +21,16 @@ const (
 	bookListSize     int    = 20 // Number of books on each smashwords list page
 )
 
-func createBookFileName(title string) string {
+func createBookFileName(title string, textFormat string) string {
 	// Remove all non-alphanumeric characters from the title
 	re := regexp.MustCompile(`[^\w]`)
 	fileName := re.ReplaceAllString(title, "")
 
-	return fmt.Sprintf("%s.txt", fileName)
+	return fmt.Sprintf("%s.%s", fileName, textFormat)
 }
 
-func downloadBook(title string, bookLink string, dataDir string) {
-	fileName := createBookFileName(title)
+func downloadBook(title string, bookLink string, dataDir string, textFormat string) {
+	fileName := createBookFileName(title, textFormat)
 	if fileName == "" {
 		log.Printf("Skipping %s since the title is all symbols (probably not English)", title)
 		return
@@ -70,7 +70,7 @@ func downloadBook(title string, bookLink string, dataDir string) {
 	log.Printf("Downloaded %s to %s\n", title, filePath)
 }
 
-func scrapeBookList(pageId int, dataDir string) {
+func scrapeBookList(pageId int, dataDir string, urlID int, textFormat string) {
 	// Create a collector for the page that lists all books
 	listCollector := colly.NewCollector(
 		colly.AllowedDomains(smashWordsURL),
@@ -98,34 +98,57 @@ func scrapeBookList(pageId int, dataDir string) {
 	// Get the text file link and download when available
 	bookCollector.OnHTML("div[id=pageContentFull]", func(e *colly.HTMLElement) {
 		title := e.ChildText("h1")
-		e.ForEach("a[title='Plain text; contains no formatting']", func(_ int, e *colly.HTMLElement) {
+		search := "a[title='Plain text; contains no formatting']"
+		if textFormat == "epub" {
+			search = "a[title='Supported by many apps and devices (e.g., Apple Books, Barnes and Noble Nook, Kobo, Google Play, etc.)']"
+		}
+
+		e.ForEach(search, func(_ int, e *colly.HTMLElement) {
 			book_link := e.Attr("href")
-			downloadBook(title, book_link, dataDir)
+			downloadBook(title, book_link, dataDir, textFormat)
 		})
 	})
 
-	westernRomanceURL := fmt.Sprintf("https://%s/books/category/%d/downloads/0/free/any/%d", smashWordsURL, westernRomanceID, pageId)
-	listCollector.Visit(westernRomanceURL)
-}
-
-func getDataDir() string {
-	dataDirPtr := flag.String("data_dir", "./data", "directory that the book files will download to")
-	flag.Parse()
-
-	return *dataDirPtr
+	smashwordsCategoryURL := fmt.Sprintf("https://%s/books/category/%d/downloads/0/free/any/%d", smashWordsURL, urlID, pageId)
+	listCollector.Visit(smashwordsCategoryURL)
 }
 
 func main() {
-	dataDir := getDataDir()
+	//flags used: -url is the url to scrape,
+	// -data_dir is the directory to save the files to
+	dataDirPtr := flag.String("data_dir", "./data",
+		"directory that the book files will download to")
 
+	urlIDPtr := flag.Int("id", 1245,
+		"The cooresponding ID for the smashswords url you want to scrape"+
+			" (in https://www.smashwords.com/books/category/1245)")
+
+	itemsPerPagePtr := flag.Int("pageitems", 20,
+		"The number of items per page on the smashwords list page")
+
+	pagesPtr := flag.Int("pages", 7,
+		"The number of pages to scrape")
+
+	textFormatPtr := flag.String("format", "txt",
+		"The format of the book to download. Options are 'txt' or 'epub'")
+	flag.Parse()
+
+	totalBooks := *itemsPerPagePtr * *pagesPtr
+
+	//log the flag parameters out to console
+	log.Printf("Scraping %d pages of %d items, (total is %d) each from smashwords url %d.\n", *pagesPtr, *itemsPerPagePtr, totalBooks, *urlIDPtr)
+	log.Printf("Selected format is %s.\n", *textFormatPtr)
+	log.Printf("Saving files to %s.\n", *dataDirPtr)
+
+	// Create a wait group to wait for all the goroutines to finish
 	wg := new(sync.WaitGroup)
 
 	// Each list page only shows `bookListSize` books so scrape each one in parallel
-	for i := 0; i <= maxPageId; i = i + bookListSize {
+	for i := 0; i < (totalBooks); i = i + *itemsPerPagePtr {
 		wg.Add(1)
 		go func(pageId int) {
 			defer wg.Done()
-			scrapeBookList(pageId, dataDir)
+			scrapeBookList(pageId, *dataDirPtr, *urlIDPtr, *textFormatPtr)
 		}(i)
 	}
 
