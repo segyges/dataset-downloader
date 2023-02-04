be1bdd0eec178a1505b224f5a45c5e7df8d0130c
rexwang8 <rexkingsbackyard@gmail.com>
2023-02-04 13:53:26 -0500

    golang epub convert

--- cmd/smashwords-downloader/go.mod
+++ cmd/smashwords-downloader/go.mod
@@ -1,6 +1,13 @@
 module github.com/coreweave/dataset-downloader/cmd/smashwords-downloader
 
-go 1.19
+go 1.18
+
+require (
+	github.com/gocolly/colly v1.2.0
+	github.com/nsf/termbox-go v1.1.1
+	github.com/taylorskalyo/goreader v0.0.0-20220528130152-945e7448ceb5
+	golang.org/x/net v0.2.0
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0 // indirect
@@ -8,18 +15,13 @@ require (
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xmlquery v1.3.13 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
-	github.com/bmaupin/go-epub v1.0.1 // indirect
-	github.com/gabriel-vasile/mimetype v1.3.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
-	github.com/gofrs/uuid v3.1.0+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50 // indirect
-	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- cmd/smashwords-downloader/main.go
+++ cmd/smashwords-downloader/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"regexp"
-	"strconv"
+	"strings"
 	"sync"
+	"time"
+	"unicode"
+
+	termbox "github.com/nsf/termbox-go"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 
 	"github.com/gocolly/colly"
+	"github.com/taylorskalyo/goreader/epub"
 )
 
 const (
@@ -22,6 +30,26 @@ const (
 	maxPageId        int    = 140
 	bookListSize     int    = 20 // Number of books on each smashwords list page
 )
+
+//parser is a part of the goreader repo for parsing epubs
+type parser struct {
+	tagStack  []atom.Atom
+	tokenizer *html.Tokenizer
+	doc       cellbuf
+	items     []epub.Item
+	sb        strings.Builder
+}
+
+//cellbuf is a part of the goreader repo for parsing epubs
+type cellbuf struct {
+	cells   []termbox.Cell
+	width   int
+	lmargin int
+	col     int
+	row     int
+	space   bool
+	fg, bg  termbox.Attribute
+}
 
 func createBookFileName(title string, textFormat string) string {
 	// Remove all non-alphanumeric characters from the title
@@ -162,17 +190,254 @@ func main() {
 	//convert epub to txt if needed
 	if *textFormatPtr == "epub" {
 
-		ConvertEpub(*dataDirPtr, *overwriteSourcePtr)
+		ConvertEpubGo(*dataDirPtr, *overwriteSourcePtr)
 	}
 }
 
-func ConvertEpub(inputdir string, overwriteSource bool) {
-	//invoke python script to convert epub to txt
-	cmd := exec.Command("python", "epubconvert.py", inputdir, inputdir, strconv.FormatBool(overwriteSource))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Println("Error: ", err)
+//A lot of the actual parsing is done with this repo: https://github.com/taylorskalyo/goreader
+func ConvertEpubGo(inputdir string, overwriteSource bool) {
+	//get all files in directory
+	files, err := ioutil.ReadDir(inputdir)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	//we time the parsing
+	start := time.Now()
+
+	//we count the number of characters
+	charCount := 0
+
+	//for each file, if it is an epub, convert it to txt
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".epub") {
+			filepath := inputdir + "/" + file.Name()
+
+			//We use the goreader library to parse the epub
+			rc, err := epub.OpenReader(filepath)
+			if err != nil {
+				panic(err)
+			}
+			defer rc.Close()
+			// The rootfile (content.opf) lists all of the contents of an epub file.
+			// There may be multiple rootfiles, although typically there is only one.
+			book := rc.Rootfiles[0]
+
+			// Print book title.
+			fmt.Println("Parsing book: ", book.Title, "(file: ", file.Name()+")")
+
+			//stringbuilder to hold the text instead of using goreader's cell system
+			var sb strings.Builder
+
+			//generate output file name and file
+			outputFileName := strings.TrimSuffix(file.Name(), ".epub") + ".txt"
+			outputFilePath := inputdir + "/" + outputFileName
+			outputFile, err := os.Create(outputFilePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer outputFile.Close()
+
+			//iterate through each chapter in the book
+			for _, itemref := range book.Spine.Itemrefs {
+				f, err := itemref.Open()
+				if err != nil {
+					panic(err)
+				}
+
+				//parse the chapter into the stringbuilder
+				sbret, err := parseText(f, book.Manifest.Items, sb)
+				if err != nil {
+					log.Fatal(err)
+				}
+				//get the string from the stringbuilder
+				chapterStr := strings.ReplaceAll(sbret.String(), "	", "")
+				charCount += len(chapterStr)
+
+				//writes to file
+				outputFile.Write([]byte(chapterStr))
+
+				// Close the itemref.
+				f.Close()
+
+				//clear the stringbuilder
+				sb.Reset()
+			}
+
+			//if overwriteSource is true, delete the original epub file
+			if overwriteSource {
+				err = os.Remove(filepath)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+		}
+
+	}
+	if charCount > 0 {
+		elapsed := time.Since(start)
+		fmt.Printf("Parsing took %s, parsed %d characters at a rate of %d characters per second.\n", elapsed, charCount, int(float64(charCount)/elapsed.Seconds()))
+	}
+}
+
+// parseText takes in html content via an io.Reader and returns a buffer
+// containing only plain text.
+func parseText(r io.Reader, items []epub.Item, sb strings.Builder) (strings.Builder, error) {
+	tokenizer := html.NewTokenizer(r)
+	doc := cellbuf{width: 80}
+	p := parser{tokenizer: tokenizer, doc: doc, items: items, sb: sb}
+	err := p.parse(r)
+	if err != nil {
+		return p.sb, err
+	}
+	return p.sb, nil
+}
+
+// parse walks an html document and renders elements to a cell buffer document.
+func (p *parser) parse(io.Reader) (err error) {
+	for {
+		tokenType := p.tokenizer.Next()
+		token := p.tokenizer.Token()
+		switch tokenType {
+		case html.ErrorToken:
+			err = p.tokenizer.Err()
+		case html.StartTagToken:
+			p.tagStack = append(p.tagStack, token.DataAtom) // push element
+			fallthrough
+		case html.SelfClosingTagToken:
+			p.handleStartTag(token)
+		case html.TextToken:
+			p.handleText(token)
+		case html.EndTagToken:
+			p.tagStack = p.tagStack[:len(p.tagStack)-1] // pop element
+		}
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			return err
+		}
+	}
+}
+
+// handleText appends text elements to the parser buffer. It filters elements
+// that should not be displayed as text (e.g. style blocks).
+func (p *parser) handleText(token html.Token) {
+	// Skip style tags
+	if len(p.tagStack) > 0 && p.tagStack[len(p.tagStack)-1] == atom.Style {
+		return
+	}
+	p.doc.style(p.tagStack)
+	//I think the appendText is needed to properly parse the tags
+	p.doc.appendText(string(token.Data))
+	p.sb.WriteString(string(token.Data))
+
+}
+
+// handleStartTag appends text representations of non-text elements (e.g. image alt
+// tags) to the parser buffer.
+func (p *parser) handleStartTag(token html.Token) {
+	switch token.DataAtom {
+	case atom.Img:
+		// Display alt text in place of images.
+		for _, a := range token.Attr {
+			switch atom.Lookup([]byte(a.Key)) {
+			case atom.Alt:
+				text := fmt.Sprintf("Alt text: %s", a.Val)
+				p.doc.appendText(text)
+				p.doc.row++
+				p.doc.col = p.doc.lmargin
+			case atom.Src:
+				for _, item := range p.items {
+					if item.HREF == a.Val {
+
+						break
+					}
+				}
+			}
+		}
+	case atom.Br:
+		p.doc.row++
+		p.doc.col = p.doc.lmargin
+	case atom.H1, atom.H2, atom.H3, atom.H4, atom.H5, atom.H6, atom.Title,
+		atom.Div, atom.Tr:
+		p.doc.row += 2
+		p.doc.col = p.doc.lmargin
+	case atom.P:
+		p.doc.row += 2
+		p.doc.col = p.doc.lmargin
+		p.doc.col += 2
+	case atom.Hr:
+		p.doc.row++
+		p.doc.col = 0
+		p.doc.appendText(strings.Repeat("-", p.doc.width))
+	}
+}
+
+// style sets the foreground/background attributes for future cells in the cell
+// buffer document based on HTML tags in the tag stack.
+func (c *cellbuf) style(tags []atom.Atom) {
+	fg := termbox.ColorDefault
+	for _, tag := range tags {
+		switch tag {
+		case atom.B, atom.Strong, atom.Em:
+			fg |= termbox.AttrBold
+		case atom.I:
+			fg |= termbox.ColorYellow
+		case atom.Title:
+			fg |= termbox.ColorRed
+		case atom.H1:
+			fg |= termbox.ColorMagenta
+		case atom.H2:
+			fg |= termbox.ColorBlue
+		case atom.H3, atom.H4, atom.H5, atom.H6:
+			fg |= termbox.ColorCyan
+		}
+	}
+	c.fg = fg
+}
+
+// appendText appends text to the cell buffer document.
+func (c *cellbuf) appendText(str string) {
+	if len(str) <= 0 {
+		return
+	}
+	if c.col < c.lmargin {
+		c.col = c.lmargin
+	}
+	runes := []rune(str)
+	/*
+		if unicode.IsSpace(runes[0]) {
+			c.space = true
+		}*/
+	scanner := bufio.NewScanner(strings.NewReader(str))
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		if c.col != c.lmargin && c.space {
+			c.col++
+		}
+		word := []rune(scanner.Text())
+		if len(word) > c.width-c.col {
+			c.row++
+			c.col = c.lmargin
+		}
+		for _, r := range word {
+			c.setCell(c.col, c.row, r, c.fg, c.bg)
+			c.col++
+		}
+		//c.space = true
+	}
+	if !unicode.IsSpace(runes[len(runes)-1]) {
+		//c.space = false
+	}
+}
+
+// setCell changes a cell's attributes in the cell buffer document at the given
+// position.
+func (c *cellbuf) setCell(x, y int, ch rune, fg, bg termbox.Attribute) {
+	// Grow in steps of 1024 when out of space.
+	for y*c.width+x >= len(c.cells) {
+		c.cells = append(c.cells, make([]termbox.Cell, 1024)...)
+	}
+	c.cells[y*c.width+x] = termbox.Cell{Ch: ch, Fg: fg, Bg: bg}
 }
